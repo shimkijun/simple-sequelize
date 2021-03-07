f903a25072ec24f0ad436c848c4bf0ab35bef677
shimkijun <shimkijun@gmail.com>
2021-03-07 14:27:27 +0900

    상세조회

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-function testMiddleWare( req, res, next ){
-    console.log('첫번째 미들웨어');
-    next();
-}
-
-function testMiddleWare2( req, res, next ){
-    console.log('두번째 미들웨어');
-    next();
-}
-
-router.get('/', testMiddleWare, testMiddleWare2 , (req,res) => {
-    res.send('admin app');
-});
-
 router.get('/products', ( _ , res) => {
     res.render( 'admin/products.html' , 
         { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
@@ -28,5 +14,7 @@ router.get('/products/write', ( _ , res) => {
 router.post('/products/write', ( req , res ) => {
     res.send(req.body);
 });
+
+router.get('/products/detail/:id', ctrl.get_products_detail );
 
 module.exports = router;
